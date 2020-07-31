e6e4d0a6371a66449e63bf439d16ccdec16e3b9d
Daniel Putzer <danielputzer.work@gmail.com>
2020-07-31 19:14:36 +0200

    test: Update flask app to test blue green deployment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "Issa Flask App!"         # which returns "hello world"
+    return "And Back To Green Again!"         # which returns "hello world"
 
 if __name__ == "__main__":        # on running python app.py
     app.run(port=80)    
